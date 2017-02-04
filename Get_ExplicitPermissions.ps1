ce528c0e148604a8e9a3878b97dce5202a79b367
Marin <marin.petkov@mail.bg>
2017-02-04 09:10:26 +0200

    Add files via upload

---
+++ Get_ExplicitPermissions.ps1
@@ -0,0 +1,21 @@
+﻿### https://www.freelancer.com/hire/Powershell/
+### Recursively get explicit permissions for NTFS Volumes
+
+### Author petkov.marin@gmail.com
+#Description: Checks which groups/users have explicit permissions to folders/subfolders/files
+#Accepts as parameter the folder or file path with option for recursive check 
+function Get-ExplicitPermissions{
+
+ param(
+ [parameter(mandatory=$true, position=1)]
+ [string]$path,
+ [switch]$Recurse 
+ )
+
+ $Objects=Get-ChildItem -Recurse:$Recurse -Path $path | select FullName
+ foreach ($object in $Objects.Fullname){
+     if ($ExpPermissions=$(get-item -path $object).getaccesscontrol().access|where {$_.IsInherited -like "False"}){
+         Write-Host "User(s)/group(s) $($ExpPermissions.IdentityReference) has/have explicit permissions on $object"
+         }
+     }
+}
