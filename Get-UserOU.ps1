bd90bf07ef265484f8c86b4b7cce3f3e0570998e
Marin <marin.petkov@mail.bg>
2017-02-04 11:00:40 +0200

    Add files via upload

---
+++ Get-UserOU.ps1
@@ -0,0 +1,29 @@
+﻿### Author petkov.marin@gmail.com
+### Description: Accept as paramter AD user name and returns its OU container 
+
+function Get-UserOU
+ {
+  param
+  (
+   [parameter(mandatory=$true)]
+   [string]$Username
+  )
+     Try{$usr=Get-ADUser $username}
+     catch{ Return "Wrong user name"}     
+            
+     $DN=$usr.DistinguishedName.split(",")
+     [array]::Reverse($DN)
+
+     for ($i=0;$i -lt ($DN | Measure-Object).count; $i++)
+     {
+        if ($DN[$i].contains("OU"))
+
+         {
+          $OU+="\"
+          $OU+=$DN[$i].substring(3)
+          
+       
+         }
+      } 
+      Write-Output "User $($usr.name) is located in OU: $OU"
+ }
